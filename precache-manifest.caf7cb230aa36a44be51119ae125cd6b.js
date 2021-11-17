a851883b01f34918f9b9ac8349a5dd7cc878f9d6
yara89 <yara3001@gmail.com>
2021-11-18 00:51:06 +0100

    Updates

---
+++ precache-manifest.caf7cb230aa36a44be51119ae125cd6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f66a7dc0bde6d393c97e466ea15dd75",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "de62d3e1c8b8cd23f09b",
+    "url": "/playground/static/css/2.5524535e.chunk.css"
+  },
+  {
+    "revision": "8b120dae542cfdc5bb72",
+    "url": "/playground/static/css/main.39e06129.chunk.css"
+  },
+  {
+    "revision": "de62d3e1c8b8cd23f09b",
+    "url": "/playground/static/js/2.34acbaac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playground/static/js/2.34acbaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b120dae542cfdc5bb72",
+    "url": "/playground/static/js/main.ba4af3b0.chunk.js"
+  },
+  {
+    "revision": "895c97baf350dc475b41",
+    "url": "/playground/static/js/runtime-main.927a30d0.js"
+  }
+]);
